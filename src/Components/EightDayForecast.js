0cef6337edd71af7160ed15eabe694ae2c04752e
Dave <daveslaw@gmail.com>
2022-01-12 15:35:58 +0200

    style improvments

--- src/Components/EightDayForecast.js
+++ src/Components/EightDayForecast.js
@@ -93,7 +93,7 @@ return (
                             <span id="cselector"  className={cSelected ? "selected" : "unselected"} onClick={cSelector}> C</span> | 
                             <span id="fselector"  className={fSelected ? "selected" : "unselected"} onClick={fSelector}> F</span>
                         </div>
-                        <img alt={index} src={"http://openweathermap.org/img/wn/" + daily.weather[0].icon + "@2x.png"}></img>
+                        {/* <img alt={index} src={"http://openweathermap.org/img/wn/" + daily.weather[0].icon + "@2x.png"}></img> */}
                         <div >{capitalize(daily.weather[0].description)}</div>
                                           
                     </div>
@@ -114,23 +114,25 @@ return (
                             </div>
                             <div id="body-container" className="flex-row body-container test-border1">
                             <div className="temp-container">
-                                <div className="flex-column test-border1">
-                                    <div>Max: {cSelected ? toCelcius(appContext.weekly.daily[keys].temp.max) : toFahrenheit(appContext.weekly.daily[keys].temp.max)} &#xb0;</div>
-                                    <div>Min: {cSelected ? toCelcius(appContext.weekly.daily[keys].temp.min) : toFahrenheit(appContext.weekly.daily[keys].temp.min)} &#xb0;</div>
+                                <div className="flex-column min-max test-border1">
+                                    <div>Max: {cSelected ? toCelcius(appContext.weekly.daily[keys].temp.max) : toFahrenheit(appContext.weekly.daily[keys].temp.max)} </div>
+                                    <div>Min: {cSelected ? toCelcius(appContext.weekly.daily[keys].temp.min) : toFahrenheit(appContext.weekly.daily[keys].temp.min)} </div>
 
                                 </div>
-                                <div className="test-border1 cf-container">
+                                <div className="test-border1 cf-container">&#xb0;
                                     <span className={cSelected ? "selected" : "unselected"} onClick={cSelector}>C</span> |
                                     <span className={fSelected ? "selected" : "unselected"} onClick={fSelector}>F</span>
                                 </div>
                             </div>                           
-                                <div id="other-info" className="test-border1 flex-column">
+                                <div id="body-info" className="test-border1 body-info flex-column">
                                     <div>Humidity {appContext.weekly.daily[keys].humidity}</div>
                                     <div>Sunrise {moment.unix(appContext.weekly.daily[keys].sunrise).format('h:mm:ss a')}</div>
                                     <div>Sunset {moment.unix(appContext.weekly.daily[keys].sunset).format('h:mm:ss a')}</div>
                                 </div>
                             </div>
-                            <img src={"http://openweathermap.org/img/wn/" + appContext.weekly.daily[keys].weather[0].icon + "@2x.png"}></img>
+                            <div>
+                                <img alt="forecast-icon" className="small-img" src={"http://openweathermap.org/img/wn/" + appContext.weekly.daily[keys].weather[0].icon + "@2x.png"}></img>
+                            </div>
 
             </div>}
             
